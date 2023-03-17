f2fff501cf09a2bbf9ce249d628b0ed185eb7b43
Jose Ángel Gumiel <jagumiel001@gmail.com>
2023-03-17 15:51:18 +0100

    03 Variables and Constants declaration in Rust

---
+++ 03-VarsAndConst/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let x = 42; // declare a variable
+    const Y: i32 = 100; // declare a constant
+
+    println!("The value of x is {}", x); // print the value of x
+    println!("The value of Y is {}", Y); // print the value of Y
+}
