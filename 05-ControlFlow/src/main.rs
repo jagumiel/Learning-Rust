0992a36ac4b0e34fadad277aa55751469129870c
Jose Ángel Gumiel <jagumiel001@gmail.com>
2023-03-19 21:27:00 +0100

    05-Flow control in rust

---
+++ 05-ControlFlow/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let x = 10;
+
+    if x < 0 {
+        println!("x is negative");
+    } else if x > 0 {
+        println!("x is positive");
+    } else {
+        println!("x is zero");
+    }
+}
