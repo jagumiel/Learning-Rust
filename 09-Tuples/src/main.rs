fa336d88b4365f25d0eb5a694779bb1331c39ebc
Jose Ángel Gumiel <jagumiel001@gmail.com>
2023-03-26 13:35:53 +0200

    09-Tuples in Rust

---
+++ 09-Tuples/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let my_tuple = (1, "Hello World", 3.141592);
+
+    println!("The first element of the tuple is: {}", my_tuple.0);
+    println!("The second element of the tuple is: {}", my_tuple.1);
+    println!("The third element of the tuple is: {}", my_tuple.2);
+}
