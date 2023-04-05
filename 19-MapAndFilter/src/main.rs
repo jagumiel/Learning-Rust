0c59cd1c8ce1def1fa5fb602dd69ec8628fa8e86
Jose Ángel Gumiel <jagumiel001@gmail.com>
2023-04-06 00:56:15 +0200

    19-Map and Filter in Rust
    Fixed.

--- 19-MapAndFilter/src/main.rs
+++ 19-MapAndFilter/src/main.rs
@@ -17,14 +17,13 @@ fn main() {
     let squared_numbers: Vec<i32> = numbers.iter().map(|x| x * x).collect();
 
     // Use filter to create a new vector with only even numbers
-    //let even_numbers: Vec<i32> = numbers.iter().filter(|x| x % 2 == 0).cloned().collect();
-
+    let even_numbers = numbers.iter().filter(|&&x| x % 2 == 0).collect::<Vec<_>>();
 
     // Use filter to create a new vector with prime even numbers
-    //let primes: Vec<i32> = numbers.into_iter().filter(|&n| is_prime(n)).collect();
+    let primes = numbers.iter().filter(|&&x| is_prime(x) == true).collect::<Vec<_>>();
 
-    println!("Original vector: {:?}", numbers);
-    println!("Squared vector: {:?}", squared_numbers);
-    //println!("Even vector: {:?}", even_numbers);
-    //println!("Primes: {:?}", primes);
+    println!("Original vector:  {:?}", numbers);
+    println!("Squared vector:   {:?}", squared_numbers);
+    println!("Even vector:      {:?}", even_numbers);
+    println!("Primes vector:    {:?}", primes);
 }
